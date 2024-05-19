1de948c1a592352e02a3402933a347b51c0b47fc
Kolapo Wariz <kolapowariz@gmail.com>
2024-05-19 17:12:32 +0100

    more

--- src/Components/Healler.jsx
+++ src/Components/Healler.jsx
@@ -1,23 +1,26 @@
-import searchIcon from '/searchIcon.png'
-import postIcon from '/postIcon.png'
-import homeIcon from '/homeIcon.png'
-import stethoIcon from '/stethoIcon.png'
+import searchIcon from '/searchSvg.svg'
+import postIcon from '/postSvg.svg'
+import homeIcon from '/homeSvg.svg'
+import stethoIcon from '/doctor.png'
+import { Link } from 'react-router-dom'
 
 
 const Healler = () => {
   return(
-    <body>
-      <header>
-        <nav className='bg-white fixed w-full'>
-          <ul className='flex justify-between items-center'>
-            <li> <img src={searchIcon} alt="Search" className='w-10 h-10'/> <span>Search</span> </li>
-            <li> <img src={postIcon} alt="Post"className='w-10 h-10' /><span>Post</span></li>
-            <li> <img src={homeIcon} alt="Healler" className='w-10 h-8' /><span>Home</span></li>
-            <li><img src={stethoIcon} alt="Stetho" className='w-10 h-10' /><span>Consultation</span></li>
+    <div className='mt-2'>
+      <header className='bg-white fixed w-full bottom-0 '>
+        <nav className=''>
+          <ul className='flex justify-around items-center text-gray-500'>
+            <li> <Link><img src={searchIcon} alt="Search"/> <span>Search</span></Link> </li>
+            <li><Link> <img src={postIcon} alt="Post" /><span>Post</span></Link></li>
+            <li> <Link><img src={homeIcon} alt="Healler" /><span>Home</span></Link></li>
+            <li className='border border-red-500'><Link className=''><img src={stethoIcon} alt="Stetho"  /><span>Consultation</span></Link></li>
           </ul>
         </nav>
       </header>
-      <main>
+      <main className='w-11/12 mx-auto mb-16 '>
+        
+        <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea doloremque doloribus suscipit saepe. Amet provident aspernatur ratione mollitia commodi, minima exercitationem, delectus veritatis dolor hic nihil dolorum, laboriosam vel dicta.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet minus, incidunt iusto in, rem eos totam placeat at, atque nobis. Perspiciatis laboriosam nisi enim illum minus, deleniti facilis beatae.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea doloremque doloribus suscipit saepe. Amet provident aspernatur ratione mollitia commodi, minima exercitationem, delectus veritatis dolor hic nihil dolorum, laboriosam vel dicta.
@@ -66,8 +69,9 @@ const Healler = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet minus, incidunt iusto in, rem eos totam placeat at, atque nobis. Perspiciatis laboriosam nisi enim illum minus, deleniti facilis beatae.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea doloremque doloribus suscipit saepe. Amet provident aspernatur ratione mollitia commodi, minima exercitationem, delectus veritatis dolor hic nihil dolorum, laboriosam vel dicta.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet minus, incidunt iusto in, rem eos totam placeat at, atque nobis. Perspiciatis laboriosam nisi enim illum minus, deleniti facilis beatae.
+        </p>
       </main>
-    </body>
+    </div>
   )
 }
 
