2c2fdcaba7d369104015a6dd6aaf87b8ef9c3a66
Kolapo Wariz <kolapowariz@gmail.com>
2024-06-17 21:06:49 +0100

    implemented search functionality

--- src/Components/main/Search.jsx
+++ src/Components/main/Search.jsx
@@ -4,13 +4,14 @@ import { AuthContext } from "../AuthContext";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { DataContext } from "../PostContext";
+import { useState } from "react";
 
 const Search = () => {
 
   const { isAuthenticated } = useContext(AuthContext);
   const navigate = useNavigate()
   const { posts, isLoading } = useContext(DataContext);
-  console.log(posts);
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -25,6 +26,8 @@ const Search = () => {
     return <div className='text-center text-3xl mt-4'>Loading...</div>
   }
 
+  const filteredPosts = posts.filter(post => post.fullName.toLowerCase().includes(search.toLowerCase()));
+
   return (
 
     <div className='mt-2 mx-auto'>
@@ -36,12 +39,14 @@ const Search = () => {
           required
           placeholder="Search"
           className="block mt-4 mx-auto w-2/3 rounded-3xl bg-gray-200 border-0 py-1.5 px-4 text-gray-900 text-md shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:text-center focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
         />
       </label>
       <Navigation />
       <main className='w-11/12 mx-auto mb-16 '>
         <ul>
-          {posts.map((posted) => (
+          {filteredPosts.map((posted) => (
             <li key={posted.userId} className='h-auto border mt-4 px-4 py-4 rounded-lg shadow-md'>
               <p>{posted.fullName}</p>
               <p>{posted.work}</p>
